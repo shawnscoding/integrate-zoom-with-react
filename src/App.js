da5a692376fd5ced45365af4c65e6a67bd80311c
shawn <shawn@salin.co.kr>
2020-11-02 21:52:00 +0900

    ready for contribution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Form from "./form/Form";
 import useStyles from "./hook/UseStyles";
 import Zoom from "./zoom/Zoom";
 
@@ -39,7 +40,7 @@ const App = () => {
     stylePath: PATH.formCssUrl,
     status: false,
   });
-  const { userName, userEmail } = config;
+  const { userName } = config;
   const { stylePath } = isSubmitted;
 
   const handleChange = (e) => {
@@ -62,44 +63,14 @@ const App = () => {
   useStyles(stylePath);
 
   return (
-    <div className="custom-zn__container">
-      <div className="custom-zm__modal">
-        <div className="custom-zm__title__box">
-          <h1 className="custom-zm__title">Welcome to Zoom</h1>
-        </div>
-        <form onSubmit={handleSubmit}>
-          <label className="custom-zm__modal__label" htmlFor="userName">
-            User Name (required*)
-            <input
-              type="text"
-              value={userName}
-              className="custom-zm__modal__input"
-              onChange={handleChange}
-              id="userName"
-              placeholder="User Name"
-            />
-          </label>
-          <label className="custom-zm__modal__label" htmlFor="userEmail">
-            Email (optional)
-            <input
-              type="email"
-              value={userEmail}
-              className="custom-zm__modal__input"
-              onChange={handleChange}
-              id="userEmail"
-              placeholder="Email"
-            />
-          </label>
-          <div className="custom-zm__btn__box">
-            <button className="custom-zm__btn" type="submit">
-              Submit
-            </button>
-          </div>
-        </form>
-
-        <Zoom isSubmitted={isSubmitted} meetConfig={config} />
-      </div>
-    </div>
+    <>
+      <Form
+        handleSubmit={handleSubmit}
+        handleChange={handleChange}
+        config={config}
+      />
+      <Zoom isSubmitted={isSubmitted} meetConfig={config} />
+    </>
   );
 };
 
